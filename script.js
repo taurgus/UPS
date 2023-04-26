a130cc825cf3d35a8ef6f8c1a862587bb928908a
taurgus <100350635+taurgus@users.noreply.github.com>
2023-04-26 18:04:48 +0300

    upsi

---
+++ script.js
@@ -0,0 +1,15 @@
+const postalCodeData = {
+    '02210': 'Olari',
+    '02250': 'Olari',
+    '00500': 'Kallio',
+  };
+
+  const form = document.querySelector('form');
+  const resultDiv = document.querySelector('#result');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const postalCode = document.querySelector('#postalCode').value;
+    const placeName = postalCodeData[postalCode];
+    resultDiv.textContent = `${postalCode} menee autoon ${placeName}.`;
+  });
