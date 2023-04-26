6b9791256345c69b8c0b69611366520747256552
taurgus <100350635+taurgus@users.noreply.github.com>
2023-04-26 18:48:38 +0300

    korjaus

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-const postalCodeData = {
-    '02210': 'Olari',
-    '02250': 'Olari',
-    '00500': 'Kallio',
-  };
+const postalCodeData = [
+    //ESPOO
+    { name: 'Otaniemi', codes: ['02100', '02110', '02130', '02150'] },
+    { name: 'Olari', codes: ['02210', '02250', ''] },
 
+    //HELSINKI
+    { name: 'Helsinki', codes: ['00100', '00120', '00130'] },
+    { name: 'Kallio', codes: ['00500', '00520', '00530'] },
+
+  ];
   const form = document.querySelector('form');
   const resultDiv = document.querySelector('#result');
 
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     const postalCode = document.querySelector('#postalCode').value;
-    const placeName = postalCodeData[postalCode];
-    resultDiv.textContent = `${placeName}`;
+    let placeName = '';
+    for (let i = 0; i < postalCodeData.length; i++) {
+      if (postalCodeData[i].codes.includes(postalCode)) {
+        placeName = postalCodeData[i].name;
+        break;
+      }
+    }
+    if (placeName) {
+      resultDiv.textContent = `${placeName}.`;
+    } else {
+      resultDiv.textContent = `Opettele kirjoittamaan ${postalCode}.`;
+    }
   });
