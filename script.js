6e8e5c43b8518c43bdf8c40e6550bbb88fcf1d38
taurgus <100350635+taurgus@users.noreply.github.com>
2023-04-27 10:28:36 +0300

    edit

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const postalCodeData = [
     //ESPOO
     { name: 'Otaniemi', codes: ['02100', '02110', '02130', '02150'] },
-    { name: 'Lepuski', codes: ['02140', '02600', '02650', '02680']},
-    { name: 'Olari', codes: ['02170', '02210', '02250', ''] },
-    { name: 'Kauniainen', codes: ['', '', '', ''] },
+    { name: 'Lepuski', codes: ['02140', '02600', '02650', '02660', '02680']},
+    { name: 'Olari', codes: ['02120', '02170', '02210', '02250', ''] },
+    { name: 'Kauniainen', codes: ['02610', '02630', '02700', '02750'] },
     { name: 'Tammisto', codes: ['', '', '', ''] },
     { name: 'Tikkurila', codes: ['', '', '', ''] },
     { name: 'ESPOO', codes: ['', '', '', ''] },
     { name: 'ESPOO', codes: ['', '', '', ''] },
 
     //HELSINKI
-    { name: 'Ruoholahti', codes: ['', '', ''] },
-    { name: 'Punavuori', codes: ['', '', ''] },
-    { name: 'Helsinki', codes: ['', '', ''] },
-    { name: 'Helsinki', codes: ['', '', ''] },
+    { name: 'Ruoholahti', codes: ['00', '00180', '00220'] },
+    { name: 'Punavuori', codes: ['00100', '00120', '00150'] },
+    { name: 'Lauttasaari', codes: ['00200', '00210', '02160'] },
+    { name: 'Esplanadi', codes: ['00100', '00120', '00130'], streets: ['Mannerheimintie', 'Aleksanterinkatu', 'Pohjoisesplanadi'] },
     { name: 'Helsinki', codes: ['', '', ''] },
     { name: 'Helsinki', codes: ['', '', ''] },
     { name: 'Helsinki', codes: ['', '', ''] },
@@ -23,6 +23,15 @@ const postalCodeData = [
     { name: 'Netlux Matinkylä', codes: ['02230', '02240', '02270'] },
     { name: 'Netlux Juvanmalmi', codes: ['', '', ''] },
     { name: 'Netlux Kivenlahti', codes: ['02260', '02300', '02320'] },
+    { name: 'Netlux Espoon keskus', codes: ['', '', ''] },
+
+  ];
+  //KATUMUUTTUJAT
+  const streetOverrides = [
+    { street: 'Mannerheimintie', location: 'Töölö' },
+    { street: 'Helsinginkatu', location: 'Kallio' },
+    
+
 
   ];
   const form = document.querySelector('form');
@@ -33,14 +42,25 @@ const postalCodeData = [
     const postalCode = document.querySelector('#postalCode').value;
     let placeName = '';
     for (let i = 0; i < postalCodeData.length; i++) {
-      if (postalCodeData[i].codes.includes(postalCode)) {
-        placeName = postalCodeData[i].name;
+      for (let j = 0; j < postalCodeData[i].codes.length; j++) {
+        if (postalCodeData[i].codes[j].includes('-')) {
+          const [start, end] = postalCodeData[i].codes[j].split('-');
+          if (postalCode >= start && postalCode <= end) {
+            placeName = postalCodeData[i].name;
+            break;
+          }
+        } else if (postalCodeData[i].codes.includes(postalCode)) {
+          placeName = postalCodeData[i].name;
+          break;
+        }
+      }
+      if (placeName) {
         break;
       }
     }
     if (placeName) {
       resultDiv.textContent = `${placeName}`;
     } else {
-      resultDiv.textContent = `Anna oikea postari tai opettele kirjoittamaan!`;
+      resultDiv.textContent = 'Anna oikea postari';
     }
   });
